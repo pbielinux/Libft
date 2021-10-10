e644898daffe8dc28be671357110a0aa23e5f3b8
pbielinux <pedrobielik@hotmail.com>
2021-10-10 23:01:52 +0100

    after push_swap

--- include/deque.h
+++ include/deque.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   deque.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbielik <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/10 22:30:32 by pbielik           #+#    #+#             */
+/*   Updated: 2021/10/10 22:30:33 by pbielik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef DEQUE_H
 # define DEQUE_H
 
@@ -7,8 +19,8 @@
 typedef struct s_deque_node
 {
 	void				*content;
-	struct s_deque_node *next;
-	struct s_deque_node *prev;
+	struct s_deque_node	*next;
+	struct s_deque_node	*prev;
 }	t_deque_node;
 
 typedef struct s_deque
@@ -18,17 +30,17 @@ typedef struct s_deque
 	struct s_deque_node	*tail;
 }	t_deque;
 
-t_deque	*deque_value(void);
-bool	deque_is_empty(t_deque *deque);
-void	deque_push_front(t_deque *deque, void *content);
-void	deque_push_back(t_deque *deque, void *content);
+t_deque			*deque_value(void);
+bool			deque_is_empty(t_deque *deque);
+void			deque_push_front(t_deque *deque, void *content);
+void			deque_push_back(t_deque *deque, void *content);
 
-void	*deque_pop_front(t_deque *deque);
-void	*deque_pop_back(t_deque *deque);
+void			*deque_pop_front(t_deque *deque);
+void			*deque_pop_back(t_deque *deque);
 
-void	*deque_peek_front(t_deque *deque);
-void	*deque_peek_back(t_deque *deque);
+void			*deque_peek_front(t_deque *deque);
+void			*deque_peek_back(t_deque *deque);
 
-t_deque_node *deque_node_value(void *content);
+t_deque_node	*deque_node_value(void *content);
 
 #endif // !DEQUE_H
--- src/deque_utils.c
+++ src/deque_utils.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   deque_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbielik <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/10 22:27:00 by pbielik           #+#    #+#             */
+/*   Updated: 2021/10/10 22:27:01 by pbielik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-t_deque_node *deque_node_value(void *content)
+t_deque_node	*deque_node_value(void *content)
 {
 	t_deque_node	*node;
-	
+
 	node = malloc(sizeof(t_deque_node));
 	omm_guard(node, __FILE__, __LINE__);
 	node->content = content;
